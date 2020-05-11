5267b6304eb0dc6ce5acb2ff13ea52c6a09d273a
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-11 16:14:48 +0100

    refactored fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,37 @@
-describe('fizzbuzz', function() {
-  it('returns fizz when number can be divided by 3', function() {
-    expect(fizzbuzz(6)).toEqual('fizz')
+describe('FizzBuzz', function() {
+  var fizzBuzz
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz()
   })
-  it('returns buzz when number can be divided by 5', function() {
-    expect(fizzbuzz(5)).toEqual('buzz')
+
+  describe('multiples of 3', function() {
+    it('returns fizz for number 3', function() {
+      expect(fizzBuzz.play(3)).toEqual('fizz')
+    })
+    it('returns fizz for number 6', function() {
+      expect(fizzBuzz.play(6)).toEqual('fizz')
+    })
   })
-  it('returns fizzbuzz when number can be divided by 3 & 5', function() {
-    expect(fizzbuzz(15)).toEqual('fizzbuzz')
+
+  describe('multiples of 5', function() {
+    it('returns buzz for number 5', function() {
+      expect(fizzBuzz.play(5)).toEqual('buzz')
+    })
+    it('returns buzz for number 10', function() {
+      expect(fizzBuzz.play(10)).toEqual('buzz')
+    })
   })
+
+  describe('multiples of 3 and 5', function() {
+    it('returns fizzbuzz for number 15', function() {
+      expect(fizzBuzz.play(15)).toEqual('fizzbuzz')
+    })
+    it('returns fizzbuzz for number 30', function() {
+      expect(fizzBuzz.play(30)).toEqual('fizzbuzz')
+    })
+  })
+
   it('returns the number otherwise', function() {
-    expect(fizzbuzz(7)).toEqual(7)
+    expect(fizzBuzz.play(7)).toEqual(7)
   })
 })
